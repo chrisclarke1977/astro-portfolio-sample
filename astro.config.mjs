c909fef79d27ab0636da2ececa8951a6bdb68237
chrisclarke1977 <chrisclarke1977@gmail.com>
2022-08-15 16:32:25 +0100

    1 file modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  adapter: netlify(),
+  integrations: [preact()],
+  publicDir: './public',
 });
